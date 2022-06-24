2430c6e6a90f142fe971abe8cddc385df9e3219c
Steffen <dickst@mathematik.uni-marburg.de>
2022-06-24 13:12:12 +0200

    updated python script

--- src/doc2desc.sh
+++ src/doc2desc.sh
@@ -1,5 +1,5 @@
 cat ./src/mass.schema.json | sed 's/\"description\"/\"description_\"/g' | sed 's/\"documentation\"/\"description\"/g' > ./mass_.schema.json
-generate-schema-doc --config-file ./src/mass-docgen-conf.yaml ./mass_.schema.json ../public/mass.md
+generate-schema-doc --config-file ./src/mass-docgen-conf.yaml ./mass_.schema.json ./public/mass.md
 rm ./mass_.schema.json
-mkdir -p ../public/schemas
-cp ./src/mass.schema.json ../public/schemas/mass.schema.json
+mkdir -p ./public/schemas
+cp ./src/mass.schema.json ./public/schemas/mass.schema.json
