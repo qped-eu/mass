08b520bfd996de2097caa1279eea785c755345ce
Steffen <dickst@mathematik.uni-marburg.de>
2022-06-24 14:11:44 +0200

    Adjusted styling

--- src/App.tsx
+++ src/App.tsx
@@ -31,24 +31,20 @@ const useStyles = makeStyles({
     backgroundColor: '#cecece',
     marginBottom: '1rem',
   },
-  resetButton: {
-    margin: 'auto !important',
-	marginBottom: '30px',
+  actionButton: {
+	marginRight: '30px',
     display: 'block !important',
   },
+  buttons: {
+	display: flex;
+    justify-content: center;
+    align-items: center;
+  }
   demoform: {
     margin: 'auto',
     padding: '1rem',
   },
 });
-
-const initialData = {
-  name: 'Send email to Adrian',
-  description: 'Confirm if you have passed the subject\nHereby ...',
-  done: true,
-  recurrence: 'Daily',
-  rating: 3,
-};
 
 const renderers = [
   ...materialRenderers,
@@ -58,7 +54,7 @@ const renderers = [
 
 const App = () => {
   const classes = useStyles();
-  const [data, setData] = useState<any>(initialData);
+  const [data, setData] = useState<any>();
   const stringifiedData = useMemo(() => JSON.stringify(data, null, 2), [data]);
 
   const clearData = () => {
@@ -100,29 +96,31 @@ const App = () => {
             />
           </div>
         </Grid>
-        <Grid item sm={6}>
+        <Grid item sm={4}>
           <Typography variant={'h4'} className={classes.title}>
             Configuration Data
           </Typography>
           <div className={classes.dataContent}>
             <pre id='boundData'>{stringifiedData}</pre>
           </div>
-          <Button
-            className={classes.resetButton}
-            onClick={copyData}
-            color='primary'
-            variant='contained'
-          >
-            Copy to clipboard
-          </Button>
-		  <Button
-            className={classes.resetButton}
-            onClick={clearData}
-            color='primary'
-            variant='contained'
-          >
-            Clear data
-          </Button>
+		  <div className={classes.buttons}>
+			<Button
+				className={classes.actionButton}
+				onClick={copyData}
+				color='primary'
+				variant='contained'
+			>
+				Copy to clipboard
+			</Button>
+			<Button
+				className={classes.actionButton}
+				onClick={clearData}
+				color='primary'
+				variant='contained'
+			>
+				Clear data
+			</Button>
+		  </div>
         </Grid>
         
       </Grid>
