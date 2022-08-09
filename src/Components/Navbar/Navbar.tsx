6e4b5fdf1fa508ba3921bfe4ed1131c79d2ce547
Steffen <dickst@mathematik.uni-marburg.de>
2022-08-09 12:34:28 +0200

    Added Navbar component

---
+++ src/Components/Navbar/Navbar.tsx
@@ -0,0 +1,62 @@
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import {useState} from 'react';
+import {Link} from 'react-router-dom';
+import './Navbar.css';
+import { IconDefinition, findIconDefinition, library } from '@fortawesome/fontawesome-svg-core';
+import { faB, fas } from '@fortawesome/free-solid-svg-icons';
+import isMobile from '../General/MobileFunctions';
+import NavItem from './NavItem';
+import Submenu from './SubMenu';
+
+library.add(fas);
+library.add(faB);
+
+const barIcon: IconDefinition = findIconDefinition({ prefix: 'fas', iconName: 'bars' });
+const timesIcon: IconDefinition = findIconDefinition({ prefix: 'fas', iconName: 'times' });
+
+const Navbar = function() {
+    const [showMobileMenu, setShowMobileMenu] = useState(false);
+    const [topScroll, setTopScroll] = useState(true);
+    const handleMobileMenuClick = () => setShowMobileMenu(!showMobileMenu);
+    const closeMobileMenu = () => setShowMobileMenu(false);
+
+    window.addEventListener('resize', ()=>{
+        if(!isMobile()){
+            setShowMobileMenu(false)
+        }
+    });
+
+    window.addEventListener('scroll', () => {
+        setTopScroll(window.scrollY < 10);
+    });
+   
+    return (
+        <>
+            <div className={showMobileMenu ? 'mobile-menu-cancel active' : 'mobile-menu-cancel'} onClick={closeMobileMenu}></div>
+            <nav className={topScroll ? "navbar unscrolled" : "navbar"}>
+                <div className="navbar-container">
+                    <Link to="/" className="navbar-logo" onClick={closeMobileMenu}>
+                        MASS
+                    </Link>
+                    <div className='menu-icon' onClick={handleMobileMenuClick}>
+                       <FontAwesomeIcon icon={showMobileMenu ? timesIcon : barIcon} className="mobileAwesome" color='#fff'/>
+                    </div>
+                    <ul className={showMobileMenu ? 'nav-menu active' : 'nav-menu'}>
+                        <NavItem link='/' callback={closeMobileMenu}>Home</NavItem>
+                        <NavItem link='/configurator' callback={closeMobileMenu}>MASS Configurator</NavItem>
+                        <NavItem link='/documentation' callback={closeMobileMenu}>MASS Documentation </NavItem>
+                        <Submenu title='MASS Checker Tutorials'>
+                            <NavItem link='/style' callback={closeMobileMenu}>Style</NavItem>
+                            <NavItem link='/semantics' callback={closeMobileMenu}>Solution Approach</NavItem>
+                            <NavItem link='/coverage' callback={closeMobileMenu}>Test Coverage</NavItem>
+                            <NavItem link='/class' callback={closeMobileMenu}>Class</NavItem>
+                            <NavItem link='/metrics' callback={closeMobileMenu}>Metrics</NavItem>
+                        </Submenu>    
+                    </ul>           
+                </div>
+            </nav>
+        </>
+    )
+}
+
+export default Navbar;
