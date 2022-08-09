ff8ecdb511baf979cd6ee53db8704e9c1536eb50
Steffen <dickst@mathematik.uni-marburg.de>
2022-08-09 12:36:42 +0200

    Reworked Markdown component to be more robust, uses new md location and uses syntax highlighting

---
+++ src/Components/Markdown/Markdown.tsx
@@ -0,0 +1,107 @@
+import ReactMarkdown from 'react-markdown';
+import remarkGfm from 'remark-gfm';
+import rehypeRaw from 'rehype-raw'
+import { useEffect, useState , useCallback, useRef } from 'react';
+import {Prism as SyntaxHighlighter} from 'react-syntax-highlighter';
+import {dark} from 'react-syntax-highlighter/dist/esm/styles/prism';
+import "./Markdown.css";
+
+const useResize = (myRef: React.RefObject<HTMLDivElement>) => {
+	const getWidth = useCallback(() => myRef?.current?.offsetWidth, [myRef]);	
+	const [width, setWidth] = useState<number | undefined>(undefined);
+
+	useEffect(() => {
+		let maxWidth = getWidth();
+		maxWidth = maxWidth === undefined ? 20 : maxWidth - 20;
+		const handleResize = () => {
+			setWidth(maxWidth);
+		};	
+		if (myRef.current) {
+			setWidth(maxWidth);
+		}
+	
+		window.addEventListener('resize', handleResize);
+
+		return () => {	
+			window.removeEventListener('resize', handleResize);
+		};
+	}, [myRef, getWidth]);
+	
+	return width && width > 25 ? width - 25 : width;
+};
+
+const Markdown = ({ mdFile }: { mdFile: string, }) => {
+    const [input, setInput] = useState<any>();
+	const divRef = useRef<HTMLDivElement>(null);
+    const maxWidth = useResize(divRef);
+	const markdownComponent = {
+		img: ({
+			alt,
+			src,
+			title,
+		}: {
+			alt?: string;
+			src?: string;
+			title?: string;
+		}) => (
+			<img 
+				alt={alt} 
+				src={src} 
+				title={title} 
+				style={{ maxWidth: maxWidth }} 
+			/>
+		),	
+		code:({node, inline, className, children, ...props}:{node:any, inline?:any, className?:any, children:any}) => {
+			const match = /language-(\w+)/.exec(className || '');
+			return !inline && match ? (
+				<SyntaxHighlighter
+				  children={String(children).replace(/\n$/, '')}
+				  language={match[1]}
+				  PreTag="div"
+				  {...props}
+				/>
+			  ) : (
+				<code className={className} {...props}>
+				  {children}
+				</code>
+			  )
+			  }
+	};
+	
+	const transformUri = function(uri: string){
+		if(uri.startsWith("http")){
+			return uri;
+		}
+		let url = uri;
+		while(url.startsWith("/")){
+			url = url.substring(1);
+		}
+		return process.env.PUBLIC_URL + "/"+ url;
+	}
+
+	useEffect(() => {
+		fetch(process.env.PUBLIC_URL +"/markdown/" + mdFile)
+		  .then((response) => response.text())
+		  .then((text) => {
+			setInput(text);
+		  });
+	  }, []);
+
+    return (
+		<div className='markdownContainer' ref={divRef}>
+			<ReactMarkdown
+				remarkPlugins={[remarkGfm]}
+				rehypePlugins={[rehypeRaw]}
+				components={markdownComponent}
+				children={input}
+				transformImageUri={
+					uri => {
+						return transformUri(uri);
+					}
+				}
+			/>
+		</div>
+	);
+}
+
+ export default Markdown;
